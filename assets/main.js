97a0d6f16b77915d4f1a5f6437ef9f1be1e8ad4b
Matheus Raduan <math.raduan@gmail.com>
2018-06-30 19:36:00 -0300

    update lines

--- assets/main.js
+++ assets/main.js
@@ -17,25 +17,12 @@ window.addEventListener("DOMContentLoaded", function() {
   authorTag.innerHTML = wrapChars(authorTag.innerHTML);
   
   anime.timeline()
-     .add({
-      targets: '.section__home__revealer',
-      easing: 'easeInOutQuad',
-      translateY: '-100%',
-      duration: 750,
-     }).add({
-      targets: '.section__home',
-      easing: 'easeInOutQuad',
-      translateY: ['50%','0%'],
-      opacity: ['0','1'],
-      duration: 700,
-      offset: '-=200'
-     })
     .add({
       targets: '.letter',
       opacity: [0,1],
       easing: 'easeInOutQuad',
       duration: 1250,
-      offset: '-=1200',
+      // offset: '-=1200',
       delay: function(el, i) {
         return 150 * (i+1)
       }
@@ -63,5 +50,50 @@ window.addEventListener("DOMContentLoaded", function() {
         return 150 * (i+1)
       }
     });
+
+
+	(function() {
+			var lineMaker = new LineMaker({
+				position: 'relative',
+				lines: [
+					{top: '15%', left: 0, width: '100%', height: 2, color: '#ddd', hidden: true},
+					{top: '85%', left: 0, width: '100%', height: 2, color: '#ddd', hidden: true},
+					{top: 0, left: '90%', width: 2, height: '100%', color: '#ddd', hidden: true},
+					{top: 0, left: '10%', width: 2, height: '100%', color: '#ddd', hidden: true},
+					{top: '15%', left: 0, width: '100%', height: 2, color: '#000', hidden: true, animation: { duration: 1000, easing: 'easeInOutExpo', delay: 0, direction: 'LeftRight' }},
+					{top: '85%', left: 0, width: '100%', height: 2, color: '#000', hidden: true, animation: { duration: 1000, easing: 'easeInOutExpo', delay: 100, direction: 'RightLeft' }},
+					{top: 0, left: '90%', width: 2, height: '100%', color: '#000', hidden: true, animation: { duration: 1000, easing: 'easeInOutExpo', delay: 200, direction: 'BottomTop' }},
+					{top: 0, left: '10%', width: 2, height: '100%', color: '#000', hidden: true, animation: { duration: 1000, easing: 'easeInOutExpo', delay: 300, direction: 'TopBottom' }}
+				]
+			});
+			
+			setTimeout(function() {
+				lineMaker.animateLineIn(4, {
+					complete: function() { lineMaker.showLine(0); }
+				});
+				lineMaker.animateLineIn(5, {
+					complete: function() { lineMaker.showLine(1); }
+				});
+				lineMaker.animateLineIn(6, {
+					complete: function() { lineMaker.showLine(2); }
+				});
+				lineMaker.animateLineIn(7, {
+					complete: function() { lineMaker.showLine(3); }
+				});
+			}, 100);
+
+			setTimeout(function() {
+				lineMaker.animateLineOut(4);
+				lineMaker.animateLineOut(5);
+				lineMaker.animateLineOut(6);
+				lineMaker.animateLineOut(7);
+			}, 2000);
+
+			setTimeout(function() {
+				lineMaker.createLine({top: '50vh', left: '21vw', width: '60vw', height: 16, color: '#E91E63', hidden: true, animation: { duration: 1000, easing: 'easeInOutExpo', delay: 300, direction: 'LeftRight' }});
+				
+				// lineMaker.animateLineIn(8);
+			}, 2000);
+		})();
   
 });
