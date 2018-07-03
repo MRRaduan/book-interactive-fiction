85fbc4229767d2d921046227b153d03128e9d3be
Matheus Raduan <math.raduan@gmail.com>
2018-07-02 23:52:44 -0300

    new home

--- assets/main.js
+++ assets/main.js
@@ -1,4 +1,4 @@
-window.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function() {
 
   let scroll = new SmoothScroll({
     updateURL: false,
@@ -10,14 +10,53 @@ window.addEventListener("DOMContentLoaded", function() {
   	return wrapTxt;
   }
   
+  titleTag = document.querySelector('.book__title');
+  titleTag.innerHTML = wrapChars(titleTag.innerHTML);
 
+  authorTag = document.querySelector('.book__author');
+  authorTag.innerHTML = wrapChars(authorTag.innerHTML);
+  
+  anime.timeline()
+    .add({
+      targets: '.section__home__revealer',
+      opacity: [1,0],
+      easing: 'easeInOutQuad',
+      duration: 7000
+    })
+    .add({
+      targets: '.letter',
+      opacity: [0,1],
+      easing: 'easeInOutQuad',
+      duration: 4250,
+      offset: '-=6500',
+      delay: function(el, i) {
+        return 275 * (i+1)
+      },
 
-complete: function(anim) {
-  setTimeout(function() {
-    let anchor = document.querySelector('#intro');
-      scroll.animateScroll(anchor, {
-        speel: 3500,
-        updateURL: false
-      });
-  }, );
-}
+    })
+    .add({
+      targets: '.letter',
+      opacity: [1,0],
+      easing: 'easeInOutQuad',
+      duration: 1125,
+      delay: function(el, i) {
+        return 90 * (i+1)
+      }
+       
+    })
+      .add({
+      targets: '.section__home__revealer',
+      opacity: [0,1],
+      easing: 'easeInOutQuad',
+      duration: 4250,
+      offset: '-=3200',
+      complete: function(anim) {
+        let anchor = document.querySelector('#intro');
+        scroll.animateScroll(anchor, {
+          speel: 3500,
+          updateURL: false
+        });
+       }
+    });
+  
+});
